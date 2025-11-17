65582075d312e295b1a07ebb7f6ac313cc23b67c
ray <rayshen1108@gmail.com>
2025-11-17 19:25:02 +0800

    Create ex5-1運算機.py

---
+++ ex5-1運算機.py
@@ -0,0 +1,16 @@
+# 數字運算範例
+
+a = input("輸入第一個數字：")
+a = int(a)  # 轉換為整數
+
+b = input("輸入第二個數字：")
+b = int(b)  # 轉換為整數
+
+
+print(f"加法：{a} + {b} = {a + b}")
+print(f"減法：{a} - {b} = {a - b}")
+print(f"乘法：{a} * {b} = {a * b}")
+print(f"除法：{a} / {b} = {a / b}")
+print(f"整數除法：{a} // {b} = {a // b}")
+print(f"餘數：{a} % {b} = {a % b}")
+print(f"次方：{a} ** {b} = {a ** b}")
